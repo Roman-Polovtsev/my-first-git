3c1968f99192d7b2a4e0e5edac30d10aecf79e16
Roman-Polovtsev <polovtsev96@yandex.ru>
2020-10-17 23:20:25 +0300

    Revert "Revert "создали подмодуль""
    This reverts commit 8d56cd2b3a1a570c865703aa1c43deebff7751c6.

---
+++ submodule.sv
@@ -0,0 +1,11 @@
+module submodule
+(
+    output logic c
+);
+
+always_latch
+begin
+    c= 0;
+end
+
+endmodule
--- verilog_git.sv
+++ verilog_git.sv
@@ -2,8 +2,14 @@ module verilog_git
 (
     input logic a
 );
+
+logic b;
  always_comb
    begin
        a= 1;
    end
+
+   submodule submodule1
+   (.c (b));
+
 endmodule
